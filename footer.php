a590d94edfe70a9431000b2d5f5d2e7f1193fd00
Baha Manai <70824369+BahaManai@users.noreply.github.com>
2024-11-10 20:03:02 +0100

    correct fault

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@
     <div class="col-4">
         <div class="card bg-warning text-white text-center">
             <div class="card-body">
-                <i class="fas fa-truck fa-3x"></i>
+                <i class="fas fa-lock fa-3x"></i>
                 <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates nobis blanditiis odit laboriosam aperiam libero.</p>
             </div>
         </div>
