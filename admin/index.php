09fac2f368226c2d3d0fdddbbd13427174c1537f
Baha Manai <70824369+BahaManai@users.noreply.github.com>
2024-11-10 17:36:34 +0100

    Préparation de background

---
+++ admin/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php include "nav.php"; ?>
+    <div class="container mt-3">
+        <h1>Liste des produits</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, consequatur autem? Iste hic quas a assumenda voluptatum. Aliquam nisi sapiente, ducimus esse, facere neque officia sint cumque ea maiores dolore.
+        Eaque, praesentium. Aperiam aliquid, voluptas minima deleniti architecto beatae quae doloribus totam aliquam et vero iure culpa quis voluptate numquam aut magnam fugit explicabo non incidunt iusto temporibus. Quibusdam, optio!
+        Quae commodi, nemo dicta neque repudiandae molestiae repellendus harum nostrum consequuntur, sed accusantium assumenda illo dolore culpa! Provident sequi iste officia amet, dicta voluptate accusantium a tempora voluptatem quis delectus!
+        Dolorum quis dignissimos et quibusdam numquam assumenda, necessitatibus doloribus recusandae dicta natus sint omnis in provident quisquam commodi qui sed! At saepe ad obcaecati explicabo quas? Facere nulla dolores error?
+        Iste et modi obcaecati? Odio facilis, quasi officiis fuga nobis ex ab quod tempora quo voluptates a inventore voluptate repellendus adipisci! Nemo, et nam praesentium autem temporibus vero ullam error?
+        Quis quam aspernatur impedit nostrum, iste autem dolor sapiente fugiat corrupti repellendus non animi blanditiis explicabo quae officia amet cum sint quidem dolorem tenetur eum, temporibus tempora et possimus? Excepturi.
+        Quisquam tenetur quia similique deleniti cumque ipsa exercitationem vitae esse hic fugiat, eligendi maxime enim unde, dolor commodi ad quae dignissimos reiciendis ea dolores asperiores ratione nihil perspiciatis voluptas. Libero.
+        Mollitia deleniti totam accusantium voluptate nihil minima incidunt eaque? Impedit accusamus consequatur cumque perspiciatis quaerat libero, ad nam harum, odio mollitia provident, laudantium officia inventore asperiores nesciunt ratione dignissimos tenetur?
+        Et laudantium nam culpa alias dolore quasi qui, quo vel, hic optio, eos at dignissimos. Eligendi, nobis saepe neque sapiente reprehenderit quas explicabo fuga voluptate consequatur hic necessitatibus illum quae?
+        Repellat nostrum sint officiis distinctio impedit praesentium fugiat magnam ea odio molestiae totam omnis, consequatur perspiciatis vel ratione quae, ab earum tempora ex? Velit totam repudiandae nisi possimus sit ullam?
+        Nostrum, dicta excepturi eos soluta doloribus placeat culpa eum maxime a dolore deserunt eveniet? Officiis hic aperiam esse voluptas asperiores est consectetur iste fugit quis velit, tempore officia atque ratione!
+        Explicabo nemo id, repellat rem vel iusto vitae blanditiis harum exercitationem vero provident officiis, aliquid, commodi fugit illum cupiditate quas numquam ipsum in asperiores soluta officia dolore! Assumenda, quas. Eos?
+        Non quos autem voluptatum aliquam! Nobis fugiat ullam magni iusto dolorem quo placeat, velit optio maxime. Quae, dolorum cupiditate? Minus tempora fugit voluptates voluptate totam, modi natus debitis voluptas iure!
+        Expedita corrupti voluptatem, odio enim quae dolorum sint laborum deleniti ut illo, quas, nam atque quam ipsum aliquid sit consequuntur consectetur voluptatibus maxime? Aperiam minima ut iure modi non pariatur.
+        Numquam deserunt consectetur obcaecati hic exercitationem, nisi, eveniet quidem assumenda dolores eum repellendus vero suscipit aspernatur. Laudantium, aliquam? Quis in doloremque saepe error repellendus qui porro eveniet. Pariatur, illo et.
+    </div>
+</body>
+
+</html>
